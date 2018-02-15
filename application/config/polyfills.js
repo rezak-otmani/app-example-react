a456bb64864aefb5e63a984d830d07af28a07aba
Rezak OTMANI <rezak@stalwart.contractors>
2018-02-15 14:18:09 +0100

    fix(config):deleting the added config, to use the config provided by create-react-app

--- application/config/polyfills.js
+++ application/config/polyfills.js
@@ -1,4 +1,4 @@
-f (typeof Promise === 'undefined') {
+if (typeof Promise === 'undefined') {
   // Rejection tracking prevents a common issue where React gets into an
   // inconsistent state due to an error, but it gets swallowed by a Promise,
   // and the user has no idea what causes React's erratic future behavior.
@@ -6,6 +6,8 @@ f (typeof Promise === 'undefined') {
   window.Promise = require('promise/lib/es6-extensions.js');
 }
 
+// fetch() polyfill for making API calls.
+require('whatwg-fetch');
 
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
--- application/src/containers/App.js
+++ application/src/containers/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Index from './Index';
 
- class App extends Components {
+ class App extends Component {
 
 render () {
  return (
