61e82bf8ee2118633077b18b51e2c9b7044303e7
Rezak OTMANI <rezak@stalwart.contractors>
2018-03-01 17:52:41 +0100

    feat(test): write unit tests

--- application/src/containers/App.test.js
+++ application/src/containers/App.test.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from "react-redux";
 import store from '../redux/store.js';
-
+import { shallow, mount, render } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
+   const tree = renderer.create(
+    <Provider store={store}><App /></Provider>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+   
+
 });
--- application/src/tests/Form.test.js
+++ application/src/tests/Form.test.js
@@ -1,22 +1,98 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 import Form from '../components/Form';
 
 
-describe('Form', function () {
-    it('should validate', function () {
-        const createFakeStore = fakeData => ({
-            getState() {
-                return fakeData;
-            },
-            subscribe(fn) {
 
-            },
-            dispatch() {
+describe('Form Component', () => {
+         
+       const wrapper = shallow(<Form />);
 
-            }
-        });
+          it('Should render Form without throwing an error', () => {
+  expect(shallow(<Form />).exists(<form className='ui form'></form>)).toBe(true) 	  })
+               
+         it('render the name input', () => {
+   	 expect(shallow(<Form />).find('[name="name"]').length), 1
+  	 })
 
-        expect(true).toBeTruthy();
-   });
-});
+         it('render the email input', () => {
+  	  expect(shallow(<Form />).find('[name="email"]').length), 1
+  	 })
+
+         it('render the subject input', () => {
+          expect(shallow(<Form />).find('[name="subject"]').length), 1
+         })
+
+         it('render the phone input', () => {
+          expect(shallow(<Form />).find('[name="phone"]').length), 1
+         })
+
+         it('render the  adresse input', () => {
+          expect(shallow(<Form />).find('[name="adresse"]').length), 1
+         })
+
+          it('render the <SubmitButton /> component', () => {
+          (wrapper.find('button[type="submit"]').length, 1);
+          });
+
+           it('renders the  <ResetButton /> component', () => {
+          (wrapper.find('button[type="reset"]').length, 1);
+          });
+     
+               
+        
+    describe('The user populates the inputs', () => {
+                 const item_name = 'john doe';
+                  const item_mail = 'blah@gmail.com';
+                  const item_subject = 'demo';
+                   const item_phone = '1234567890';
+                   const item_adresse = 'my adresse';
+
+       it(' populate name input', () => {
+           const input = wrapper.find('[name="name"]');
+           wrapper.simulate('change', {
+           target: { value: item_name }
+        })
+        })
+
+          it(' populate email input', () => {
+           const input = wrapper.find('[name="email"]');
+           wrapper.simulate('change', {
+           target: { value: item_mail }
+        })
+        })
+
+
+         it(' populate subject input', () => {
+           const input = wrapper.find('[name="subject"]');
+           wrapper.simulate('change', {
+           target: { value: item_subject }
+        })
+        })
+
+        it(' populate phone input', () => {
+           const input = wrapper.find('[name="phone"]');
+           wrapper.simulate('change', {
+           target: { value: item_phone }
+        })
+       })
+
+         it(' populate adresse input', () => {
+           const input = wrapper.find('[name="adresse"]');
+           wrapper.simulate('change', {
+           target: { value: item_adresse }
+        })
+      })
+   
+
+      })
+ 
+
+
+
+   })
+
+
+
+
