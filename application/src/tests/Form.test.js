497978ad440940af08072086b8644e0d057406c8
Rezak OTMANI <rezak@stalwart.contractors>
2018-03-02 17:28:02 +0100

    feat(edit): write unit tests

--- application/src/tests/Form.test.js
+++ application/src/tests/Form.test.js
@@ -6,11 +6,23 @@ import Form from '../components/Form';
 
 
 describe('Form Component', () => {
-         
-       const wrapper = shallow(<Form />);
+        
+  let wrapper;
 
-          it('Should render Form without throwing an error', () => {
-  expect(shallow(<Form />).exists(<form className='ui form'></form>)).toBe(true) 	  })
+  beforeEach(() => {
+    wrapper = shallow(
+      <Form />
+    );
+  });
+
+   
+
+
+     describe('Render Form Component', () => {
+
+
+        it('Should render Form without throwing an error', () => {
+  expect(shallow(<Form />).exists(<form className='ui form'></form>)).toBe(true)})
                
          it('render the name input', () => {
    	 expect(shallow(<Form />).find('[name="name"]').length), 1
@@ -40,59 +52,60 @@ describe('Form Component', () => {
           (wrapper.find('button[type="reset"]').length, 1);
           });
      
-               
+ });              
         
-    describe('The user populates the inputs', () => {
-                 const item_name = 'john doe';
-                  const item_mail = 'blah@gmail.com';
-                  const item_subject = 'demo';
-                   const item_phone = '1234567890';
-                   const item_adresse = 'my adresse';
-
-       it(' populate name input', () => {
-           const input = wrapper.find('[name="name"]');
-           wrapper.simulate('change', {
-           target: { value: item_name }
-        })
-        })
-
-          it(' populate email input', () => {
-           const input = wrapper.find('[name="email"]');
-           wrapper.simulate('change', {
-           target: { value: item_mail }
-        })
-        })
 
 
-         it(' populate subject input', () => {
-           const input = wrapper.find('[name="subject"]');
-           wrapper.simulate('change', {
-           target: { value: item_subject }
-        })
-        })
+           describe('The user populates the inputs', () => {
+                   const name = 'john';
+                   const email = 'example@gmail.com';
+                   const subject = 'demo';
+                   const phone = '1234567890';
+                   const adresse = 'my adresse';
 
-        it(' populate phone input', () => {
-           const input = wrapper.find('[name="phone"]');
-           wrapper.simulate('change', {
-           target: { value: item_phone }
-        })
-       })
 
-         it(' populate adresse input', () => {
-           const input = wrapper.find('[name="adresse"]');
-           wrapper.simulate('change', {
-           target: { value: item_adresse }
-        })
-      })
-   
+    it('should respond to change event and change the state of the Form Component', () => {
+    beforeEach(() => {
+      const input1 = wrapper.find('Field[name="name"]').first();
+      const input2 = wrapper.find('Field[name="email"]').first();
+      const input3 = wrapper.find('Field[name="subject"]').first();
+      const input4 = wrapper.find('Field[name="phone"]').first();
+      const input5 = wrapper.find('Field[name="adresse"]').first();
+      input1.simulate('change', {target: { value: name } });
+      input2.simulate('change', {target: { value: email } });
+      input3.simulate('change', {target: { value: subject } });
+      input4.simulate('change', {target: { value: phone } });
+      input5.simulate('change', {target: { value: adresse } });
+         expect(wrapper.state().name).toEqual(name);
+         expect(wrapper.state().email).toEqual(email);
+         expect(wrapper.state().subject).toEqual(subject);
+         expect(wrapper.state().phone).toEqual(phone);
+         expect(wrapper.state().adresse).toEqual(adresse);
 
-      })
- 
+    });
+});      
+    
+    
+     describe('and then submits the form', () => {
+      beforeEach(() => {
+        const form = wrapper.find('form').first();
+        form.simulate('submit', {
+          preventDefault: () => {},
+        });
+      });      
+        
+         });
 
 
 
-   })
+             
 
 
 
 
+});
+
+
+
+
+});
