3bd1b2f28d17be64dab6b7f559be712f12308765
Rezak OTMANI <rezak@stalwart.contractors>
2018-03-06 17:45:13 +0100

    feat(edit): write units tests

--- application/src/tests/Form.test.js
+++ application/src/tests/Form.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 import Form from '../components/Form';
-import { Field, reduxForm } from 'redux-form'
 
 
 describe('Form Component', () => {
@@ -28,67 +27,91 @@ describe('Form Component', () => {
         it('Should render Form without throwing an error', () => {
   expect(shallow(<Form />).exists(<form className='ui form'></form>)).toBe(true)})
               
-         it('render the name input', () => {
-          expect(wrapper.find('Field[name="name"]').length), 1
+         it('render the ame input', () => {
+         
+          const name = wrapper.find("Field[name='name']").first();
+          expect((name).length).toBe(1);
 
   	 });
-
+         
          it('render the email input', () => {
-  	  expect(wrapper.find('[name="email"]').length), 1
+
+           const email = wrapper.find("Field[name='email']").first();
+           expect((email).length).toBe(1);
+
   	 })
 
          it('render the subject input', () => {
-          expect(wrapper.find('[name="subject"]').length), 1
+            
+            const subject = wrapper.find("Field[name='subject']").first();
+            expect((subject).length).toBe(1);
          })
 
          it('render the phone input', () => {
-          expect(wrapper.find('[name="phone"]').length), 1
+            
+            const phone = wrapper.find("Field[name='phone']").first();
+            expect((phone).length).toBe(1);
          })
 
          it('render the  adresse input', () => {
-          expect(wrapper.find('[name="adresse"]').length), 1
+              
+              const adresse = wrapper.find("Field[name='adresse']").first();
+              expect((adresse).length).toBe(1);
          })
 
           it('render the <SubmitButton /> component', () => {
-          (wrapper.find('button[type="submit"]').length, 1);
+             
+              const submit = wrapper.find("button[type='submit']").first();
+              expect((submit).length).toBe(1);
           });
 
            it('renders the  <ResetButton /> component', () => {
-          (wrapper.find('button[type="reset"]').length, 1);
+                
+              const reset = wrapper.find("button[type='reset']").first();
+              expect((reset).length).toBe(1);
           });
      
- });              
+               
         
 
 
-           describe('The user populates the inputs', () => {
-                   const name = 'john';
+ describe('The user populates the inputs', () => {
+            
+            const name = 'john';
                    const email = 'example@gmail.com';
                    const subject = 'demo';
                    const phone = '1234567890';
                    const adresse = 'my adresse';
 
-
-    it('should respond to change event and change the state of the Form Component', () => {
-    beforeEach(() => {
+beforeEach(() => {
+     const name = wrapper.find("Field[name='name']").first();
+               
+          
+      const input1 = wrapper.find("Field[name='name']").first();
+      const input2 = wrapper.find("Field[name='email']").first();
+      const input3 = wrapper.find("Field[name='subject']").first();
+      const input4 = wrapper.find("Field[name='phone']").first();
+      const input5 = wrapper.find("Field[name='adresse']").first();
+        
       
-      const input1 = wrapper.find('[name="name"]').first();
-      const input2 = wrapper.find('[name="email"]').first();
-      const input3 = wrapper.find('[name="subject"]').first();
-      const input4 = wrapper.find('[name="phone"]').first();
-      const input5 = wrapper.find('[name="adresse"]').first();
+     
       input1.simulate('change', {target: { value: name } });
       input2.simulate('change', {target: { value: email } });
       input3.simulate('change', {target: { value: subject } });
       input4.simulate('change', {target: { value: phone } });
       input5.simulate('change', {target: { value: adresse } });
+        
+});         
+
+  it('should respond to change event and change the state of the Form component', () => {
+
          expect(wrapper.state().name).toEqual(name);
          expect(wrapper.state().email).toEqual(email);
          expect(wrapper.state().subject).toEqual(subject);
          expect(wrapper.state().phone).toEqual(phone);
          expect(wrapper.state().adresse).toEqual(adresse);
-
-    });
+        
+   
     }); 
     
   });
@@ -98,15 +121,15 @@ describe('Form Component', () => {
      describe('and then submits the form', () => {
       
 
-      beforeEach(() => {   
-    
-           
-           const form = wrapper.find('form').first();
-           form.simulate('submit', { preventDefault: () => {}, });
+           beforeEach(() => {
+        const form = wrapper.find('form').first();
+        form.simulate('submit', {
+          preventDefault: () => {},
+        });
       });
-
-
-       
+    
+             
+     
 
         });
 
@@ -114,5 +137,5 @@ describe('Form Component', () => {
          
           
                   
-
+});
 });
