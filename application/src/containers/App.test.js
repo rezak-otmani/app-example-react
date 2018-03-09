566127f623c50ca8b111b0b9fdece37d8733e721
Rezak OTMANI <rezak@stalwart.contractors>
2018-03-09 17:45:14 +0100

    feat(edit): write units tests

--- application/src/containers/App.test.js
+++ application/src/containers/App.test.js
@@ -3,13 +3,65 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from "react-redux";
 import store from '../redux/store.js';
-import { shallow, mount, render } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+
+describe('App Component', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <App />
+    );
+  });
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+           it('it should render the Home section', () => {
+             const home = wrapper.find('Home').first();
+             expect(home.length).toEqual(1);
+           });
+           
+           it('it should render the Product section', () => {
+             const home = wrapper.find('Product').first();
+             expect(home.length).toEqual(1);
+           });
+
+            it('it should render the About section', () => {
+             const home = wrapper.find('About').first();
+             expect(home.length).toEqual(1);
+           });
+
+             it('it should render the Featured section', () => {
+             const home = wrapper.find('Featured').first();
+             expect(home.length).toEqual(1);
+           });
+
+            it('it should render the Contact section', () => {
+             const home = wrapper.find('Contact').first();
+             expect(home.length).toEqual(1);
+           });
+           
+             it('it should render the Demo section', () => {
+             const home = wrapper.find('Demo').first();
+             expect(home.length).toEqual(1);
+           });
+
+             it('it should render the ButtonScroll section', () => {
+             const home = wrapper.find('ButtonScrol').first();
+             expect(home.length).toEqual(1);
+           });
+
+
+
    const tree = renderer.create(
     <Provider store={store}><App /></Provider>
   ).toJSON();
